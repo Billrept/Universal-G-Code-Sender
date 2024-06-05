ec7be6e32346c9699ae38e0c667d9565e0c66473
phurintbyp <66011159@kmitl.ac.th>
2024-06-05 12:17:05 +0700

    Add files via upload
    Added pluginTest

---
+++ ugs-platform/pluginTest/target/generated-sources/annotations/com/willwinder/plugintest/Bundle.java
@@ -0,0 +1,26 @@
+package com.willwinder.plugintest;
+/** Localizable strings for {@link com.willwinder.plugintest}. */
+class Bundle {
+    /**
+     * @return <i>windowClass</i>
+     * @see windowClassTopComponent
+     */
+    static String CTL_windowClassAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_windowClassAction");
+    }
+    /**
+     * @return <i>windowClass Window</i>
+     * @see windowClassTopComponent
+     */
+    static String CTL_windowClassTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_windowClassTopComponent");
+    }
+    /**
+     * @return <i>This is a windowClass window</i>
+     * @see windowClassTopComponent
+     */
+    static String HINT_windowClassTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_windowClassTopComponent");
+    }
+    private Bundle() {}
+}
